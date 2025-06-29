d24a21c421cd5de4a232614cffda73aa2a726b99
CATyPH67 <031kirill2a@mail.ru>
2025-06-29 15:11:40 +0300

    add spring server and mvn

--- src/main/java/com/example/simpleServer/SimpleServer.java
+++ src/main/java/com/example/simpleServer/SimpleServer.java
@@ -1,3 +1,5 @@
+package com.example.simpleServer;
+
 import java.io.*;
 import java.net.*;
 
---
+++ src/main/java/com/example/springServer/SpringServer.java
@@ -0,0 +1,12 @@
+package com.example.springServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+@SpringBootApplication
+public class SpringServer {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringServer.class, args);
+    }
+}
